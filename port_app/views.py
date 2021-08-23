a3a213db5fee4dad365ea49030d9536866220887
JakeZ34 <JzBaseball34@hotmail.com>
2021-08-23 15:41:31 -0700

    updates

--- port_app/views.py
+++ port_app/views.py
@@ -3,6 +3,9 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'index.html')
 
+#def me(request):
+    #return render(request, 'me.html')
+
 #def proj(request):
     #return render(request, 'proj.html')
 
