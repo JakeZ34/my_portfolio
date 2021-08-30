552b72bc28dd07136f41450fda8119b72ef63353
JakeZ34 <JzBaseball34@hotmail.com>
2021-08-30 13:31:09 -0700

    newnew

--- port_app/urls.py
+++ port_app/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
+    path('portal', views.portal),
 ]
--- port_app/views.py
+++ port_app/views.py
@@ -3,6 +3,9 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'index.html')
 
+def portal(request):
+    return render(request, 'projportal.html')
+
 #def me(request):
     #return render(request, 'me.html')
 
